c82efc4832a14b475ba6efa5acd15a0b66023656
missixx <61702374+missixx@users.noreply.github.com>
2020-03-07 00:00:28 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -29,6 +29,8 @@ $(function () {
 
     
     $(".reviews__slider").slick({
+      nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+      prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
       infinite: true,
       speed: 300,
       slidesToShow: 1,
